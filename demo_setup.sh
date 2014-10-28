18147339a73f29ced9c4c7fad51cc252a613259a
Rajat Chopra <rchopra@redhat.com>
2014-10-28 14:11:04 -0700

    fix ssh config issues; git clone origin repo and vagrant up is the responsibility of this script now

--- demo_setup.sh
+++ demo_setup.sh
@@ -1,25 +1,37 @@
 #!/bin/bash
 
+basedir=`dirname $0`
+
+if [ ! -d origin ]; then
+  git clone https://github.com/openshift/origin
+fi
+cd origin
+export OPENSHIFT_DEV_CLUSTER=1 
+vagrant up
+
 # Check for / setup SSH config file
+mkdir -p ~/.ssh
 if [ -f ~/.ssh/config ]; then
   cat ~/.ssh/config | grep 10.245
   if [ $? -ne 0 ]; then
     echo "" >> ~/.ssh/config
-    cat ssh_config >> ~/.ssh/config
+    vagrant ssh-config >> ~/.ssh/config
   fi
 else
-  cp ssh_config ~/.ssh/config
+  vagrant ssh-config >> ~/.ssh/config
 fi
 chmod 600 ~/.ssh/config
 
+cd ..
+
 # Copy the git config to the master instance
-scp git_config demo_master:~/.gitconfig
+scp ${basedir}/git_config master:~/.gitconfig
 
 # Run each setup script
 echo "Running setup on Vagrant hosts."
-ssh demo_master 'bash -s' < master_setup.sh &
-ssh demo_minion-1 'bash -s' < minion_setup.sh &
-ssh demo_minion-2 'bash -s' < minion_setup.sh &
+ssh master 'bash -s' < ${basedir}/master_setup.sh &
+ssh minion-1 'bash -s' < ${basedir}/minion_setup.sh &
+ssh minion-2 'bash -s' < ${basedir}/minion_setup.sh &
 wait
 echo "Setup completed."
 exit
